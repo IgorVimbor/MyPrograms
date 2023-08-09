7ae7de9a59ef66a6587471633ee2b479b9a1ccca
Igor Vimbor <vimbor73@gmail.com>
2023-08-09 23:40:57 +0300

    Начало работы с модулем interface

--- Порешайка_3-4кл_в разработке/create_interface/interface.py
+++ Порешайка_3-4кл_в разработке/create_interface/interface.py
@@ -1,1 +1,11 @@
 # Модуль создания интерфейса приложения с использованием Tkinter
+
+from factory_examples.example_12 import Example_12
+from factory_examples.example_34 import Example_34
+
+class My_func():
+    def __init__(self) -> None:
+        pass
+
+    def clear(self, *args):
+        pass
