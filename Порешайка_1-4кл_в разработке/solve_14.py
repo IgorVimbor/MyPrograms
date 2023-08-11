8404681f80af80051ca1ae9c6d1d46e70f93c979
Igor Vimbor <vimbor73@gmail.com>
2023-08-11 23:04:04 +0300

    Устранены мелкие неточности, добавлены комментарии

--- Порешайка_1-4кл_в разработке/solve_14.py
+++ Порешайка_1-4кл_в разработке/solve_14.py
@@ -41,7 +41,7 @@ def get_test():
     # если флажки не поставлены
     if not any(mask):
         messagebox.showinfo('ВНИМАНИЕ!', 'Не выбран вид примера!\n\n'
-                            'ПОСТАВЬТЕ ГАЛОЧКУ В ОДНОМ ИЛИ НЕСКОЛЬКТХ ПОЛЯХ.')
+                            'ПОСТАВЬТЕ ГАЛОЧКУ В ОДНОМ ИЛИ НЕСКОЛЬКИХ ПОЛЯХ.')
     # если поставлены флажки в 1-2 классе и ни одного в 3-4 классе
     if any([flg_1, flg_2]) and not any([flg_3, flg_4, flg_5]):
         entry_2.insert(0, obj_12.get_example(*mask[:2]))
@@ -62,8 +62,11 @@ def check_answer():
 
     # считываем из поля для примеров сам пример, удаляем знак "=" и пробел
     test = entry_2.get().split(' =')[0]       # например: 12 - 10 + 6
+
+    # заменяем знаки Юникода '×' и '∶'
     test = test.replace('∶', '/') if '∶' in test else test
     test = test.replace('×', '*') if '×' in test else test
+
     try:
         result_to_test = int(eval(test))      # решаем считанный пример
         answer = int(entry_3.get())           # считываем ответ из поля для ответов
@@ -86,8 +89,8 @@ def check_answer():
             entry_7.delete(0, END)            # удаляем старое значение счетчика из поля
             entry_7.insert(0, f'{cnt_bad}')   # вставляем новое значение счетчика
     except:
-        messagebox.showinfo('ОШИБКА!', 'Нельзя проверить то, чего нет )))\n'
-                            'Нажмите кнопку "Нажми и появится пример".')
+        messagebox.showinfo('ОШИБКА!', 'Нельзя проверить то, чего нет )))\n\n'
+                            'НАЖМИТЕ КНОПКУ "Нажми и появится пример".')
 
 cnt_good = cnt_bad = 0   # счетчики решеных примеров
 
@@ -104,9 +107,10 @@ screenheight = window.winfo_screenheight()
 window.geometry('%dx%d+%d+%d'%(width, heigh, (screenwidth-width)/2, (screenheight-heigh)/2))
 window.title(f"ПОРЕШАЙКА by IgorVimbor\n{' ' * 20} Примеры по математике")    # текст заголовка
 
-# пустая строка
-lbl = Label(window, text="", font=("Arial Bold", 2))
-lbl.grid(row=0, column=0)
+# формируем пустые строки (label) для красоты окна
+for i in (0, 2, 4, 7, 9, 11):
+    lbl = Label(window, text='', font=("Arial Bold", 2))
+    lbl.grid(row=i, column=0)
 
 # формируем и размещаем строку с текстом "Число-ограничение:"
 lbl_1 = Label(window, text="Число-ограничение:", font=("Arial Bold", 11))
@@ -117,69 +121,63 @@ entry_1 = Entry(font=("Arial Bold", 18), width=3)
 entry_1.grid(row=1, column=3, sticky='w')
 entry_1.focus()   # переводим курсор в поле для ввода числа-ограничения
 
-# пустая строка
-lbl = Label(window, text="", font=("Arial Bold", 2))
-lbl.grid(row=2, column=0)
-
+# формируем и размещаем первый фрейм с надписью для строк и кнопок-флажков
 frm_1 = LabelFrame(window, text="1-2 класс", font=("Arial Bold", 15), labelanchor='n')
 frm_1.grid(row=3, column=0, columnspan=4, sticky='n', padx=10)
 
+# формируем и размещаем второй фрейм с надписью для строк и кнопок-флажков
 frm_2 = LabelFrame(window, text="3-4 класс", font=("Arial Bold", 15), labelanchor='n')
 frm_2.grid(row=3, column=5, columnspan=9, padx=5)
 
-# формируем и размещаем строку с текстом "Сложение/вычитание 2-х чисел:"
+# формируем и размещаем в первом фрейме строку с текстом "Сложение/вычитание 2-х чисел:"
 lbl_2 = Label(frm_1, text="Сложение/вычитание 2-х чисел:", font=("Arial Bold", 11))
 lbl_2.grid(row=0, column=0, sticky='e')
 
-# формируем и размещаем кнопку-флажок для 2-х чисел
+# формируем и размещаем в первом фрейме кнопку-флажок для 2-х чисел
 entry_bnt_1_state = BooleanVar()  # создаем объект для хранения логического значения виджета
 entry_bnt_1_state.set(False)      # устанавливаем значение по умолчанию (флажок выключен)
 entry_bnt_1 = Checkbutton(frm_1, width=3, variable=entry_bnt_1_state)  # создаем объект кнопки-флажка
-entry_bnt_1.grid(row=0, column=1, sticky='w')                   # размещаем кнопку-флажок
+entry_bnt_1.grid(row=0, column=1, sticky='w')    # размещаем кнопку-флажок
 
-# формируем и размещаем строку с текстом "3-х чисел:"
+# формируем и размещаем в первом фрейме строку с текстом "3-х чисел:"
 lbl_3 = Label(frm_1, text="3-х чисел:", font=("Arial Bold", 11))
 lbl_3.grid(row=1, column=0, sticky='e')
 
-# формируем и размещаем кнопку-флажок для 3-х чисел
+# формируем и размещаем в первом фрейме кнопку-флажок для 3-х чисел
 entry_bnt_2_state = BooleanVar()  # создаем объект для хранения логического значения виджета
 entry_bnt_2_state.set(False)      # устанавливаем значение по умолчанию (флажок выключен)
 entry_bnt_2 = Checkbutton(frm_1, width=3, variable=entry_bnt_2_state)  # создаем объект кнопки-флажка
-entry_bnt_2.grid(row=1, column=1, sticky='w')                   # размещаем кнопку-флажок
+entry_bnt_2.grid(row=1, column=1, sticky='w')    # размещаем кнопку-флажок
 
-# формируем и размещаем строку с текстом "Сложение/вычитание со скобками:"
+# формируем и размещаем во втором фрейме строку с текстом "Сложение/вычитание со скобками:"
 lbl_4 = Label(frm_2, text="Сложение/вычитание со скобками:", font=("Arial Bold", 11))
 lbl_4.grid(row=0, column=0, sticky='e')
 
-# формируем и размещаем кнопку-флажок для cложения/вычитания со скобками
+# формируем и размещаем во втором фрейме кнопку-флажок для cложения/вычитания со скобками
 entry_bnt_3_state = BooleanVar()  # создаем объект для хранения логического значения виджета
 entry_bnt_3_state.set(False)      # устанавливаем значение по умолчанию (флажок выключен)
 entry_bnt_3 = Checkbutton(frm_2, width=3, variable=entry_bnt_3_state)  # создаем объект кнопки-флажка
-entry_bnt_3.grid(row=0, column=1, sticky='w')                   # размещаем кнопку-флажок
+entry_bnt_3.grid(row=0, column=1, sticky='w')    # размещаем кнопку-флажок
 
-# формируем и размещаем строку с текстом "Умножение/деление 2-х чисел:"
+# формируем и размещаем во втором фрейме строку с текстом "Умножение/деление 2-х чисел:"
 lbl_5 = Label(frm_2, text="Умножение/деление 2-х чисел:", font=("Arial Bold", 11))
 lbl_5.grid(row=1, column=0, sticky='e')
 
-# формируем и размещаем кнопку-флажок для умножения/деления 2-х чисел
+# формируем и размещаем во втором фрейме кнопку-флажок для умножения/деления 2-х чисел
 entry_bnt_4_state = BooleanVar()  # создаем объект для хранения логического значения виджета
 entry_bnt_4_state.set(False)      # устанавливаем значение по умолчанию (флажок выключен)
 entry_bnt_4 = Checkbutton(frm_2, width=3, variable=entry_bnt_4_state)  # создаем объект кнопки-флажка
-entry_bnt_4.grid(row=1, column=1, sticky='w')                   # размещаем кнопку-флажок
+entry_bnt_4.grid(row=1, column=1, sticky='w')    # размещаем кнопку-флажок
 
-# формируем и размещаем строку с текстом "Смешанные примеры:"
+# формируем и размещаем во втором фрейме строку с текстом "Смешанные примеры:"
 lbl_5 = Label(frm_2, text="Смешанные примеры:", font=("Arial Bold", 11))
 lbl_5.grid(row=2, column=0, sticky='e')
 
-# формируем и размещаем кнопку-флажок для смешанных примеров
+# формируем и размещаем во втором фрейме кнопку-флажок для смешанных примеров
 entry_bnt_5_state = BooleanVar()  # создаем объект для хранения логического значения виджета
 entry_bnt_5_state.set(False)      # устанавливаем значение по умолчанию (флажок выключен)
 entry_bnt_5 = Checkbutton(frm_2, width=3, variable=entry_bnt_5_state)  # создаем объект кнопки-флажка
-entry_bnt_5.grid(row=2, column=1, sticky='w')                   # размещаем кнопку-флажок
-
-# пустая строка
-lbl = Label(window, text="", font=("Arial Bold", 2))
-lbl.grid(row=4, column=0)
+entry_bnt_5.grid(row=2, column=1, sticky='w')    # размещаем кнопку-флажок
 
 # формируем и размещаем строку с текстом "Реши пример:"
 lbl_6 = Label(window, text="Реши пример:", font=("Arial Bold", 15))
@@ -193,17 +191,13 @@ entry_2.grid(row=6, column=2, columnspan=8)
 entry_3 = Entry(font=("Arial Bold", 35), width=3)
 entry_3.grid(row=6, column=10)
 
-# пустая строка
-lbl = Label(window, text="", font=("Arial Bold", 4))
-lbl.grid(row=7, column=0)
-
 # формируем, привязываем к функции get_test и размещаем кнопку "Нажми и появится пример"
 btn_1 = Button(window,
                text="Нажми \nи появится пример",  # текст на кнопке
                bg='linen',                        # цвет фона кнопки
                activebackground='peach puff',     # цвет кнопки при нажатии на нее
                font=("Arial Bold", 12),           # тип и размер шрифта
-               command=get_test)                      # на какую команду реагирует
+               command=get_test)                  # на какую команду реагирует
 btn_1.grid(row=8, column=3, columnspan=5)
 
 # формируем, привязываем к функции check_answer и размещаем кнопку "Проверить"
@@ -214,10 +208,6 @@ btn_2 = Button(window,
                font=("Arial Bold", 15),
                command=check_answer)
 btn_2.grid(row=8, column=8, columnspan=9)
-
-# пустая строка
-lbl = Label(window, text="", font=("Arial Bold", 4))
-lbl.grid(row=9, column=0)
 
 # формируем и размещаем строку с текстом "Результат:"
 lbl_7 = Label(window, text="Результат:", font=("Arial Bold", 15))
@@ -230,10 +220,6 @@ entry_4.grid(row=10, column=2, columnspan=8)
 # формируем и размещаем поле для смайликов
 entry_5 = Entry(font=("Arial Bold", 37), width=2, justify='center')
 entry_5.grid(row=10, column=10)
-
-# пустая строка
-lbl = Label(window, text="", font=("Arial Bold", 2))
-lbl.grid(row=11, column=0)
 
 # формируем и размещаем строки подписей полей "Правильно" и "Не правильно"
 lbl_10 = Label(window, text="Правильно", font=("Arial Bold", 12))
